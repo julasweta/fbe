27d2b36bcaf239574faecd80335540bb169fa435
julasweta <julasweta@ukr.net>
2025-08-18 22:37:05 +0300

    add;telegram-module

--- backend/src/modules/order/order.module.ts
+++ backend/src/modules/order/order.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { PrismaService } from '../../../prisma/prisma.service';
+import { TelegramModule } from '../telegram/telegram.module';
 
 @Module({
+  imports: [TelegramModule],
   controllers: [OrderController],
   providers: [OrderService, PrismaService],
 })
